4da935a8c42f31ddcb7b914b75c7cabda6dbf373
paeqw <paeqw12@gmail.com>
2024-05-08 14:38:46 +0200

    idk

---
+++ app/src/main/java/paeqw/app/helpers/SharedPreferencesHelper.java
@@ -0,0 +1,36 @@
+package paeqw.app.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import paeqw.app.models.Space;
+
+public class SharedPreferencesHelper {
+
+    private static final String PREF_NAME = "WaterYourPlantsPrefs";
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
+
+    public SharedPreferencesHelper(Context context) {
+        this.sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        this.gson = new Gson();
+    }
+
+    public void saveSpaces(List<Space> spaces) {
+        String json = gson.toJson(spaces);
+        sharedPreferences.edit().putString("spaces", json).apply();
+    }
+
+    public List<Space> getSpaces() {
+        String json = sharedPreferences.getString("spaces", null);
+        if (json == null) {
+            return new ArrayList<>();
+        }
+        Type type = new TypeToken<List<Space>>() {}.getType();
+        return gson.fromJson(json, type);
+    }
+}
